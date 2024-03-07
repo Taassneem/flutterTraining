3a43fd48e28a5641563e08f64e1dda0ffb647b03
Taassneem <tasnemeadwan87@gmail.com>
2024-03-07 17:54:32 +0000

    add tasks for session two

---
+++ session_two/lib/session_two.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
+void main() {
+
+  //! Task one
+  double radius = 2;
+  double pi = 3.14;
+  double area = pi * radius * radius;
+  print('Area of circule is $area');
+  
+  //! Task two
+  print('Enter fahrenheit degree');
+  double fahrenheit;
+  fahrenheit = double.parse(stdin.readLineSync()!);
+  double celsius = 5 / 9 * (fahrenheit - 32);
+  stdout.writeln('Celsius is $celsius');
+  
+  //! Task three
+  double rating1 = 25.3,
+      rating2 = 351,
+      rating3 = 96.7,
+      average = (rating1 + rating3 + rating2) / 3;
+  double averageRating = average;
+  print('The average is $averageRating');
+
+}
